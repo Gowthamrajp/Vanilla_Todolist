a184bb2e0c3f497d9de21986f8cd60f321ae94e6
Gowtham Raj <gowthrj@amazon.com>
2022-06-25 09:10:01 +0530

    added Local storage Functionality

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Task loader - Checks local storage for stored tasks and loads tasks if found. 
+let tasks = [];
+taskImporter();
+tasks.forEach(element=> loadTodo(element)); 
+
 // selectors 
 
 document.querySelector('form').addEventListener('submit',handleSubmitForm);
@@ -13,6 +18,8 @@ function handleSubmitForm(e)
     {
         addTodo(input.value);
     }
+    else alert("Please add some task!");
+
     input.value='';
 }
 function handleClickCheckOrDelete(e)
@@ -26,11 +33,17 @@ function handleClickCheckOrDelete(e)
 function handleClickClearAll(e)
 {
     document.querySelector('ul').innerHTML = '';
+    tasks=[];
+    localStorage.clear();
+
 }
 
 //helper functions
 function addTodo(todo)
-{
+{   
+    tasks.push(todo); 
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+    
     let ul =document.querySelector('ul');
     let li =document.createElement('li');
 
@@ -42,7 +55,19 @@ function addTodo(todo)
     li.classList.add('todo-list-item')
     ul.appendChild(li);
 }
+function loadTodo(todo)
+{   
+    let ul =document.querySelector('ul');
+    let li =document.createElement('li');
 
+    li.innerHTML = `
+        <span class="todo-item">${todo}</span>
+        <button name="checkButton"><i class="fas fa-check-square"></i></button>
+        <button name="deleteButton"><i class="fas fa-trash"></i></button>
+    `;
+    li.classList.add('todo-list-item')
+    ul.appendChild(li);
+}
 function checkTodo(e)
 {
    let item = e.target.parentNode;
@@ -53,5 +78,21 @@ function checkTodo(e)
 function deleteTodo(e)
 {
    let item = e.target.parentNode;
+   tasks.forEach(element=> 
+   {
+    if(element===item.children[0].innerText)
+    {
+        tasks.splice(tasks.indexOf(element),1);
+        
+    }  
+   });
+
+   localStorage.setItem('tasks',JSON.stringify(tasks));
    item.remove(); 
+}
+function taskImporter()
+{
+    if(JSON.parse(localStorage.getItem('tasks'))!=null)
+    tasks=JSON.parse(localStorage.getItem('tasks'));  
+    
 }
